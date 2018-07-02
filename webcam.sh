4e86d29d94207a0869686041ceff9f7ee88f63ee
Luigi DI Rado <40144238+gi-diana@users.noreply.github.com>
2018-07-02 17:01:33 +0200

    select camera by passing argumen

--- webcam.sh
+++ webcam.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
 
-gst-launch-1.0 -e  v4l2src ! videoconvert ! video/x-raw,format=YUY2,width=1280,height=720,framerate=30/1 ! jpegenc ! rtpjpegpay ! udpsink host=$1 port=$2
+gst-launch-1.0 -e  v4l2src device=/dev/video$1 ! videoconvert ! video/x-raw,format=YUY2,width=1280,height=720,framerate=30/1 ! jpegenc ! rtpjpegpay ! udpsink host=$2 port=$3
